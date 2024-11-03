465cff10255987656426cb9872cd3553640abd0e
Emmanuel DK Dolo <emmanueldkdolo1993@gmail.com>
2024-11-03 14:24:30 +0000

    successfully add the drop menu to all views

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
- 
+
+//Dropdown menu for the nav element on small screen 
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('nav ul');
+  
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('show');
+    });
+  
